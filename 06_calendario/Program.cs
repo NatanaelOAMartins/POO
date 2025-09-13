6827e8631ea3a7856ca25806c2e00d025c3ea08e
Natanael Martins <natanael1337martins@gmail.com>
2025-09-12 21:44:19 -0300

    Atualização do Calendario

---
+++ 06_calendario/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Net.Http.Headers;
+
+using System.Security.Cryptography;
+
+using System.Text;
+
+using System.Threading.Tasks;
+using _06_calendario;
+
+namespace _06_Calendario
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+            //Console.Write("Digite o mês (1..12)");
+
+            //int mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o ano: ");
+
+            int ano = int.Parse(Console.ReadLine());
+
+            Calendario calendario;
+            for (int mes = 1; mes <= 12; mes++)
+
+            {                        
+                calendario= new Calendario(ano, mes);
+                calendario.ImprimirCalendario();
+                Console.WriteLine("\n\n");
+
+            }
+
+            //Espera o usuário teclar qualquer tecla
+
+            Console.ReadKey();
+
+
+        }
+
+    
+
+        public static void gerarCalendario(int ano, int mes, out DateTime primeiroDiaMes, out int[,] calendario)
+        {
+            //Descobre a quantidade de dias de um mês
+            int diasDoMes = DateTime.DaysInMonth(ano, mes);
+            //Descobre o dia da semana do primeiro dia do mês
+            // 0 = Domingo - 6 = Sábado
+            //Gera o primeiro dia do mês e ano informado pelo usuário
+            primeiroDiaMes = new DateTime(ano, mes, 1);
+            int diaSemanaInicio = (int)primeiroDiaMes.DayOfWeek;
+            //Matriz de 6 semanas e 7 dias 
+            calendario = new int[6, 7];
+            int dia = 1;
+            //Preenche a matriz com os dias do mês
+            for (int semana = 0; semana < 6; semana++)
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++)
+                {
+                    if (semana == 0 && diaSemana < diaSemanaInicio)
+                    {
+                        calendario[semana, diaSemana] = 0;
+                    }
+                    else if (dia <= diasDoMes)
+                    {
+                        calendario[semana, diaSemana] = dia;
+                        dia++;
+                    }
+                }
+            }
+        }
+
+        
+
+    }
+
+}
