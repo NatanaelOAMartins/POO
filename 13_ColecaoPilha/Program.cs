2f398445c0d444e56bf806f0200082e76832e0ac
Natanael Martins <natanael1337martins@gmail.com>
2025-09-26 20:59:18 -0300

    Criação do Stack<int>

---
+++ 13_ColecaoPilha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ColecaoPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numeros = new Stack<int>();
+            Console.WriteLine("Digite 5 números inteiros:");
+            for (int i = 1; i < 5; i++)
+            {
+                Console.Write($"Número {i}:");
+                if (int.TryParse(Console.ReadLine(), out int valor))
+                {
+                    numeros.Push(valor);
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido, tente novamente!");
+                        i--;
+                }
+            }
+            Console.WriteLine($"\nÚltimo número inserido: {numeros.Peek()}");
+        }
+    }
+}
