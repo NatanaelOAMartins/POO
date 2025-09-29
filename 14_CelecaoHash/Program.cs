bf982c5f4fa296a5c078a76b1add69786d8a0b49
Natanael Martins <natanael1337martins@gmail.com>
2025-09-29 19:21:05 -0300

    Criação de List<string> para armazenar nomes

---
+++ 14_CelecaoHash/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_CelecaoHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numeros = new HashSet<int>();
+            int[] valores = { 1, 2, 3, 4, 5, 3, 2, 6, 7, 1 };
+
+            foreach (int v in valores)
+                numeros.Add(v);
+
+            Console.WriteLine("_____Números sem duplicatas_____");
+                foreach (int n in numeros)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ 15_ColecaoDicionario/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ColecaoDicionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> alunos = new Dictionary<int, string>();
+
+            Console.WriteLine("Cadastro de alunos (RA e Nome):");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write($"Digite o RA do aluno {i}: ");
+                int ra = int.Parse(Console.ReadLine());
+                
+                string nome;
+                do
+                {
+                    Console.Write("Digite o nome do aluno: ");
+                    nome = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(nome) || nome.Length < 3)
+                        Console.WriteLine("Nome inválido! Deve ter pelo menos 3 caracteres.");
+                } while (string.IsNullOrWhiteSpace(nome) || nome.Length < 3);
+
+                alunos.Add(ra, nome);
+            }
+
+            Console.WriteLine("\n--- Alunos cadastrados ---");
+            foreach (var aluno in alunos)
+                Console.WriteLine($"RA: {aluno.Key}, Nome: {aluno.Value}");
+        }
+    }
+}
