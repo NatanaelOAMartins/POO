a629673531c815f122e7057f2a12bca7aa224d87
Natanael Martins <natanael1337martins@gmail.com>
2025-10-06 20:43:50 -0300

    Tratamento Exceção

---
+++ 19_TratamntoExcecao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_TratamentoExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Digite o numero inteiro:");
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Numero invalido, informe apenas valor inteiro!\nPressione qualquer tecla para continuar ");                  
+                }
+                finally
+                {
+                    Console.Clear();
+                }
+
+            }
+            if (numero % 2 == 0)
+            
+                Console.WriteLine("O numero é par");            
+            else
+                Console.WriteLine("O numero é impar");
+        }
+    }
+}
